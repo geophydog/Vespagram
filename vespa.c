028234de675e4fdad6c287c44f117a39dfbec96e
geophydog <geophydogvon@gmail.com>
2017-06-30 12:47:00 +0800

    modified

--- vespa.c
+++ vespa.c
@@ -31,32 +31,34 @@ float sign( float x ) {
 
 /*----------------------------------------------------------------------------MAIN PROGRAM-------------------------------------------------------------------*/
 int main( int argc, char *argv[] ) {
-    int i, j, size = 256, sac_npts, shift_index, count = 0, sta_index = 0;
-    float *data, f1, f2, Nth_root, time, center_lon = 0., center_lat = 0., dx, dy, shift_time, delta,time_start, time_end, **allamp, **coordi, *amp;
+    int i, j, size = 256, sac_npts, shift_index, count = 0, sta_index = 0, begin_index, end_index, beam_npts;
+    float *data, t1, t2, f1, f2, Nth_root, time, center_lon = 0., center_lat = 0., dx, dy, shift_time, delta,time_start, time_end, **allamp, **coordi, *beam;
     SACHEAD hd;
     char *ss, *slow = {"slow"}, *backazimuth = {"baz"};
     FILE *fin, *fout, *fbp, *fp;
 
-    if ( argc != 11 ) {
-        fprintf(stderr,"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
-        fprintf(stderr,"++    USAGE: vespa <filelist> <f1> <f2> <ID:slow/baz> <slow/baz> <slow_low/baz_low> <slow_high/baz_high> <step_len> <Nth_root> <output>   ++\n");
-        fprintf(stderr,"++            return output_results of 3 columns: col1: time; col2: slowness/backazimuth col3: amplitude                                  ++\n");
-        fprintf(stderr,"++            <filelist>           [1] file containing every SAC format file                                                              ++\n");
-        fprintf(stderr,"++            <f1>                 [2] low limitation of corner frequency of bandpass filter                                              ++\n");
-        fprintf(stderr,"++            <f2>                 [3] high limitation of corner frequency of bandpass filter                                             ++\n");
-        fprintf(stderr,"++            <ID:slow/baz>        [4] \"slow\" or \"baz\", which means fix slowness/baz and scan baz/slowness                                ++\n");
-        fprintf(stderr,"++            <slow/baz>           [5] fixed slowness/backazimuth                                                                         ++\n");
-        fprintf(stderr,"++            <slow_low/baz_low>   [6] low limitation of scanning sloness or backazimuth                                                  ++\n");
-        fprintf(stderr,"++            <slow_high/baz_high> [7] high limitation of scanning slowness or backazimuth                                                ++\n");
-        fprintf(stderr,"++            <step_len>           [8] step length of scanning slowness/backazimuth                                                       ++\n");
-        fprintf(stderr,"++            <Nth_root>           [9] Nth root slant-stacking, here specially, N consistant with 1 means linear stacking                 ++\n");
-        fprintf(stderr,"++            <output>             [10]file saving outputting results: col1: time col2: slowness/baz col3: amplitude                      ++\n");
-        fprintf(stderr,"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    if ( argc != 13 ) {
+        fprintf(stderr,"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+        fprintf(stderr,"++    USAGE: vespa <filelist> <t1> <t2> <f1> <f2> <ID:slow/baz> <slow/baz> <slow_low/baz_low> <slow_high/baz_high> <step_len> <Nth_root> <output>    ++\n");
+        fprintf(stderr,"++            return output_results of 3 columns: col1: time; col2: slowness/backazimuth col3: amplitude                                             ++\n");
+        fprintf(stderr,"++            <filelist>           [1]  file containing every SAC format file                                                                        ++\n");
+        fprintf(stderr,"++            <t1>                 [2]  beginning time of executing Velocity Spectral Analisys                                                       ++\n");
+        fprintf(stderr,"++            <t2>                 [3]  ebding time of executing Velocity Spectal Analysis                                                           ++\n");
+        fprintf(stderr,"++            <f1>                 [4]  low limitation of corner frequency of bandpass filter                                                        ++\n");
+        fprintf(stderr,"++            <f2>                 [5]  high limitation of corner frequency of bandpass filter                                                       ++\n");
+        fprintf(stderr,"++            <ID:slow/baz>        [6]  \"slow\" or \"baz\", which means fix slowness/baz and scan baz/slowness                                          ++\n");
+        fprintf(stderr,"++            <slow/baz>           [7]  fixed slowness/backazimuth                                                                                   ++\n");
+        fprintf(stderr,"++            <slow_low/baz_low>   [8]  low limitation of scanning sloness or backazimuth                                                            ++\n");
+        fprintf(stderr,"++            <slow_high/baz_high> [9]  high limitation of scanning slowness or backazimuth                                                          ++\n");
+        fprintf(stderr,"++            <step_len>           [10] step length of scanning slowness/backazimuth                                                                 ++\n");
+        fprintf(stderr,"++            <Nth_root>           [11] Nth root slant-stacking, here specially, N consistant with 1 means linear stacking                           ++\n");
+        fprintf(stderr,"++            <output>             [12] file saving outputting results: col1: time col2: slowness/baz col3: amplitude                                ++\n");
+        fprintf(stderr,"+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
         exit(1);
     }
 
-    fin = fopen(argv[1],"r"); fout = fopen(argv[10],"w"); fp = fopen("plot.sh","w");
-    f1 = atof(argv[2]); f2 = atof(argv[3]); ss = (char*) malloc(size); Nth_root = atof(argv[10]);
+    fin = fopen(argv[1],"r"); fout = fopen(argv[12],"w"); fp = fopen("plot.sh","w");
+    t1 = atof(argv[2]); t2 = atof(argv[3]); f1 = atof(argv[4]); f2 = atof(argv[5]); ss = (char*) malloc(size); Nth_root = atof(argv[11]);
 
     time_start = clock();
     while ( fgets(ss, size, fin) ) {
@@ -65,11 +67,11 @@ int main( int argc, char *argv[] ) {
         delta = hd.delta; sac_npts = hd.npts; center_lon += hd.stlo; center_lat += hd.stla; count += 1;
     }
     free(data); fclose(fin);
-    center_lon /= count; center_lat /= count;
+    center_lon /= count; center_lat /= count; begin_index = (int) (t1/delta); end_index = (int) (t2/delta); beam_npts = (int) ((t2-t1)/delta);
 
 /*--------------------------------------------------------allocate dynamic memories to array "allamp", "coordi" and "amp"---------------------------*/
     allamp = (float**) malloc(sizeof(float*) * count); coordi = (float**) malloc(sizeof(float*) * count);
-    amp = (float*) malloc(sizeof(float) * sac_npts);
+    beam = (float*) malloc(sizeof(float) * sac_npts);
     for ( i = 0; i < count; i ++ ) {
         allamp[i] = (float*) malloc(sizeof(float) * sac_npts);
         coordi[i] = (float*) malloc(sizeof(float) * 2);
@@ -97,51 +99,76 @@ int main( int argc, char *argv[] ) {
     system("rm *.bp"); free(data); fclose(fin);
 
 /*--------------------------------------------------------fixed slowness and scanning backazimuth---------------------------------------------------*/
-    if ( !strcmp(slow, argv[4]) ) {
+    if ( !strcmp(slow, argv[6]) ) {
         float slowness, baz_low, baz_high, baz_step, baz_scan, slow_x, slow_y;
-        slowness = atof(argv[5]); baz_low = atof(argv[6]); baz_high = atof(argv[7]); baz_step = atof(argv[8]);
+        slowness = atof(argv[7]); baz_low = atof(argv[8]); baz_high = atof(argv[9]); baz_step = atof(argv[10]);
         baz_scan = baz_low;
         while ( baz_scan <= baz_high ) {
-            for ( i = 0; i < sac_npts; i ++ ) amp[i] = 0.;
+            for ( i = 0; i < beam_npts; i ++ ) beam[i] = 0.;
             for ( i = 0; i < count ; i++ ) {
                 slow_x = slowness * cos( (90-baz_scan)/180.*PI ); slow_y = slowness * sin( (90-baz_scan)/180.*PI );
                 dx = center_lon - coordi[i][0]; dy = center_lat - coordi[i][1];
                 shift_time = slow_x * dx + slow_y * dy; shift_index = (int) (shift_time/delta);
-                for ( j = 0; j < sac_npts; j ++ ) {
-                    if ( j + shift_index >= 0 && j + shift_index < sac_npts ) amp[j] += sign(allamp[i][j+shift_index])*pow(allamp[i][j+shift_index],Nth_root)/count;
-                    else amp[j] = 0.;
+                for ( j = 0; j < beam_npts; j ++ ) {
+                    if ( (begin_index + j + shift_index >= 0) && (begin_index + j + shift_index < sac_npts) )
+                        beam[j] += sign(allamp[i][begin_index+j+shift_index])*pow(allamp[i][begin_index+j+shift_index],Nth_root)/count;
+                    else beam[j] = 0.;
                 }
             }
-            for ( j = 0; j < sac_npts; j ++ ) {
-                time = j * delta;
-                fprintf(fout, "%f %f %f\n", time, baz_scan, fabs(amp[j]));
+            for ( j = 0; j < beam_npts; j ++ ) {
+                time = t1 + j * delta;
+                fprintf(fout, "%f %f %f\n", time, baz_scan, fabs(beam[j]));
             }
             baz_scan += baz_step;
         }
+    /*----------------------------------------------------Scanning backazimuth:save plot script in file "plot.sh"----------------------- -----------*/
+    fprintf(fp,"R=%f/%f/%f/%f\n", t1, t2, baz_low, baz_high);
+    fprintf(fp,"J=X9i/7i\nPS=%f~%f.ps\nPDF=%f~%f.pdf\n", t1, t2, t1, t2);
+    fprintf(fp,"gmt surface %s -R$R -I%f/%f -G%s.grd\n", argv[12], delta*10, baz_step/2, argv[12]);
+    fprintf(fp,"gmt grd2cpt %s.grd -Cjet -Z>tmp.cpt\n", argv[12]);
+    fprintf(fp,"gmt psxy -R$R -J$J -K -T>$PS\n");
+    fprintf(fp,"gmt grdimage %s.grd -R -J -K -O -Bx%d+l\"Time(sec)\" -By%f+l\"Backazimuth(deg)\" -BWSen>>$PS\n", argv[12], (int)((t2-t1)/10), baz_high/10);
+    fprintf(fp,"gmt psscale -Ctmp.cpt -R -J -K -O -D9.4i/3.5i/15/0.8 -Ba0.4:\"Spectral Power\":>>$PS\n");
+    fprintf(fp,"gmt psxy -R -J -O -T>>$PS\nps2pdf $PS $PDF\n");
+    fprintf(fp,"rm tmp.* gmt.*\n"); fprintf(fp,"evince $PDF\n");
+    fclose(fp);
+
     }
 
 /*--------------------------------------------------------fixed backazimuth and scanning slowness---------------------------------------------------*/
-    else if ( !strcmp(backazimuth, argv[4]) ) {
+    else if ( !strcmp(backazimuth, argv[6]) ) {
         float backazimuth, slow_low, slow_high, slow_step, slow_scan, slow_x, slow_y;
-        backazimuth = atof(argv[5]); slow_low = atof(argv[6]); slow_high = atof(argv[7]); slow_step = atof(argv[8]);
+        backazimuth = atof(argv[7]); slow_low = atof(argv[8]); slow_high = atof(argv[9]); slow_step = atof(argv[10]);
         slow_scan = slow_low;
         while ( slow_scan <= slow_high ) {
-            for ( i = 0; i < sac_npts; i ++ ) amp[i] = 0.;
+            for ( i = 0; i < beam_npts; i ++ ) beam[i] = 0.;
             for ( i = 0; i < count; i ++ ) {
                 slow_x = slow_scan * cos( (90.-backazimuth)/180.*PI ); slow_y = slow_scan * sin( (90.-backazimuth)/180.*PI );
                 dx = center_lon - coordi[i][0]; dy = center_lat - coordi[i][1];
                 shift_time = slow_x * dx + slow_y * dy; shift_index = (int) (shift_time/delta);
-                for ( j = 0; j < sac_npts; j ++ ) {
-                    if ( (j + shift_index >= 0) && (j + shift_index < sac_npts) ) amp[j] += sign(allamp[i][j+shift_index])*pow(allamp[i][j+shift_index],Nth_root)/count;
-                    else amp[j] += 0.;
+                for ( j = 0; j < beam_npts; j ++ ) {
+                    if ( (begin_index + j + shift_index >= 0) && (begin_index + j + shift_index < sac_npts) )
+                        beam[j] += sign(allamp[i][begin_index+j+shift_index])*pow(allamp[i][begin_index+j+shift_index],Nth_root)/count;
+                    else beam[j] += 0.;
                 }
             }
-            for ( j = 0; j < sac_npts; j ++ ) {
-                time = j * delta;
-                fprintf(fout,"%f %f %f\n", time, slow_scan, fabs(amp[j]));
+            for ( j = 0; j < beam_npts; j ++ ) {
+                time = t1 + j * delta;
+                fprintf(fout,"%f %f %f\n", time, slow_scan, fabs(beam[j]));
             }
             slow_scan += slow_step;
         }
+    /*----------------------------------------------------Scanning slowness:save plot script in file "plot.sh"--------------------------------------*/
+    fprintf(fp,"R=%f/%f/%f/%f\n", t1, t2, slow_low, slow_high);
+    fprintf(fp,"J=X9i/7i\nPS=%f~%f.ps\nPDF=%f~%f.pdf\n", t1, t2, t1, t2);
+    fprintf(fp,"gmt surface %s -R$R -I%f/%f -G%s.grd\n", argv[12], delta*10, slow_step/2, argv[12]);
+    fprintf(fp,"gmt grd2cpt %s.grd -Cjet -Z>tmp.cpt\n", argv[12]);
+    fprintf(fp,"gmt psxy -R$R -J$J -K -T>$PS\n");
+    fprintf(fp,"gmt grdimage %s.grd -R -J -K -O -Bx%d+l\"Time(sec)\" -By%f+l\"Slowness(sec/deg)\" -BWSen>>$PS\n", argv[12], (int)((t2-t1)/10), slow_high/10);
+    fprintf(fp,"gmt psscale -Ctmp.cpt -R -J -K -O -D9.4i/3.5i/15/0.8 -Ba0.4:\"Spectral Power\":>>$PS\n");
+    fprintf(fp,"gmt psxy -R -J -O -T>>$PS\n"); fprintf(fp,"ps2pdf $PS $PDF\n");
+    fprintf(fp,"rm tmp.* gmt.*\n"); fprintf(fp,"evince $PDF\n");
+    fclose(fp);
     }
 
 /*--------------------------------------------------------inputting ID (slow/baz) is not proper and exit--------------------------------------------*/
@@ -160,7 +187,7 @@ int main( int argc, char *argv[] ) {
     for ( i = 0; i < count; i++ ) {
         free(allamp[i]); free(coordi[i]);
     }
-    free(allamp); free(coordi); free(amp); fclose(fout); fclose(fp);
+    free(allamp); free(coordi); free(beam); fclose(fout);
 
 
 
