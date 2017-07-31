0ef5c05fe9e7060866dcda4482eecb9c0c8a52ee
geophydog <geophydogvon@gmail.com>
2017-07-31 20:38:32 +0800

    modified

--- vespa.c
+++ vespa.c
@@ -137,7 +137,7 @@ int main( int argc, char *argv[] ) {
     fprintf(fp,"gmt makecpt -Cbr.cpt -T-1/1.0/0.01 -Z>tmp.cpt\n");
     fprintf(fp,"gmt psxy -R$R -J$J -K -T>$PS\n");
     fprintf(fp,"gmt grdimage %s.grd -R -J -K -O -Bx%d+l\"Time(sec)\" -By%f+l\"Backazimuth(deg)\" -BWSen -Ctmp.cpt>>$PS\n", argv[12], (int)((t2-t1)/10), (baz_high-baz_low)/10.);
-    fprintf(fp,"gmt psscale -Ctmp.cpt -R -J -K -O -D9.4i/3i/12/0.8 -Ba0.1:\"Normalized Spectral Power\":>>$PS\n");
+    fprintf(fp,"gmt psscale -Ctmp.cpt -R -J -K -O -D9.4i/3i/15.1/1 -Ba0.2:\"Normalized Spectral Power\":>>$PS\n");
     fprintf(fp,"gmt psxy -R -J -O -T>>$PS\nps2pdf $PS $PDF\n");
     fprintf(fp,"gmt psconvert -Tg -A -P $PS\n");
     fprintf(fp,"rm tmp.* gmt.*\n"); fprintf(fp,"evince $PDF\n");
@@ -181,7 +181,7 @@ int main( int argc, char *argv[] ) {
     fprintf(fp,"gmt makecpt -Cbr.cpt -T-1/1./0.01 -Z>tmp.cpt\n");
     fprintf(fp,"gmt psxy -R$R -J$J -K -T>$PS\n");
     fprintf(fp,"gmt grdimage %s.grd -R -J -K -O -Bx%d+l\"Time(sec)\" -By%f+l\"Slowness(sec/deg)\" -BWSen -Ctmp.cpt>>$PS\n", argv[12], (int)((t2-t1)/10), (slow_high-slow_low)/10.);
-    fprintf(fp,"gmt psscale -Ctmp.cpt -R -J -K -O -D9.4i/3i/12/0.8 -Ba0.1:\"Normalized Spectral Power\":>>$PS\n");
+    fprintf(fp,"gmt psscale -Ctmp.cpt -R -J -K -O -D9.4i/3i/15.1/1 -Ba0.2:\"Normalized Spectral Power\":>>$PS\n");
     fprintf(fp,"gmt psxy -R -J -O -T>>$PS\n"); fprintf(fp,"ps2pdf $PS $PDF\n");
     fprintf(fp,"gmt psconvert -Tg -A -P $PS\n");
     fprintf(fp,"rm tmp.* gmt.*\n"); fprintf(fp,"evince $PDF\n");
