9b80cde8fb23216535718f9bb7ac5b49cd27e5c0
Xuping Feng <2514037718@qq.com>
2017-12-07 20:06:44 +0800

    modified part of title font in GMT plot script

--- vespa.c
+++ vespa.c
@@ -132,8 +132,8 @@ int main( int argc, char *argv[] ) {
     /*----------------------------------------------------Scanning backazimuth:save plot script in file "plot.sh"----------------------- -----------*/
     fprintf(fp,"R=%f/%f/%f/%f\n", t1, t2, baz_low, baz_high);
     fprintf(fp,"J=X9i/6i\nPS=%f~%f.ps\nPDF=%f~%f.pdf\n", t1, t2, t1, t2);
-    fprintf(fp,"gmt gmtset FONT_LABEL 25,27,black\n");
-    fprintf(fp,"gmt gmtset FONT_TITLE 30,27,black\n");
+    fprintf(fp,"gmt gmtset FONT_LABEL 25,5,black\n");
+    fprintf(fp,"gmt gmtset FONT_TITLE 30,5,black\n");
     fprintf(fp,"awk '{print $1,$2,$3/%f}' %s >tmp.txt\n", peak, argv[12]);
     fprintf(fp,"gmt surface tmp.txt -R$R -I%f/%f -G%s.grd\n", delta*10, baz_step/2, argv[12]);
     fprintf(fp,"gmt makecpt -Cpolar.cpt -T-1/1.0/0.01 -Z>tmp.cpt\n");
@@ -178,8 +178,8 @@ int main( int argc, char *argv[] ) {
     /*----------------------------------------------------Scanning slowness:save plot script in file "plot.sh"--------------------------------------*/
     fprintf(fp,"R=%f/%f/%f/%f\n", t1, t2, slow_low, slow_high);
     fprintf(fp,"J=X9i/6i\nPS=%f~%f.ps\nPDF=%f~%f.pdf\n", t1, t2, t1, t2);
-    frpintf(fp,"gmt gmtset FONT_LABEL 25,27,black\n");
-    fprintf(fp,"gmt gmtset FONT_TITLE 30,27,black\n");
+    frpintf(fp,"gmt gmtset FONT_LABEL 25,5,black\n");
+    fprintf(fp,"gmt gmtset FONT_TITLE 30,5,black\n");
     fprintf(fp,"awk '{print $1,$2,$3/%f}' %s >tmp.txt\n", peak, argv[12]);
     fprintf(fp,"gmt surface tmp.txt -R$R -I%f/%f -G%s.grd\n", delta*10, slow_step/2, argv[12]);
     fprintf(fp,"gmt makecpt -Cpolar.cpt -T-1/1./0.01 -Z>tmp.cpt\n");
